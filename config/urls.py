d5cfda6931c5e20401e411e05fb8fcab98c5c654
nibmangit <nibretumengaw@gmail.com>
2026-04-04 00:14:25 +0300

    fix: fix the file configuration to upload the file and access them in the frontend

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # Add this
+from django.conf.urls.static import static # Add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('api/summery/', include('summery.urls')),
     path('api/progress/', include('progress.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- uploads/serializers.py
+++ uploads/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
+import os
 from .models import Upload
 
 class UploadSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
     class Meta:
         model = Upload
-        fields = ['id', 'session', 'file', 'file_type','source_type','is_active', 'created_at']
+        fields = ['id', 'session','name', 'file', 'file_type','source_type','is_active', 'created_at']
         read_only_fields = ['id', 'created_at', 'file_type']
 
     def validate_file(self, value):
@@ -12,3 +14,7 @@ class UploadSerializer(serializers.ModelSerializer):
         if not value.name.endswith('.pdf'):
             raise serializers.ValidationError("Only PDF files are supported for now.")
         return value
+    
+    def get_name(self, obj):
+        # Extracts 'Geography_Notes.pdf' from the file path
+        return os.path.basename(obj.file.name)
